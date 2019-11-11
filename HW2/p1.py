c7b62afdaeb89581d6ffefc3ea3c25443b034b7a
MortalHappiness <b07901069@ntu.edu.tw>
2019-11-12 00:34:27 +0800

    complete HW2

--- HW2/p1.py
+++ HW2/p1.py
@@ -1,9 +1,23 @@
 import numpy as np
 from util import mod_inv
 
+# ==============================
+
+LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ_.,?!'
+
+# ==============================
+
+def text_to_matrix(text):
+    while len(text) % 3 != 0:
+        text += text[-1]
+    text = [LETTERS.index(i) for i in text]
+    text = np.reshape(text, (-1, 3)).T
+    return text
+
+def matrix_to_text(matrix):
+    return ''.join([LETTERS[x] for x in matrix.T.flatten()])
 
 def decode(cipher, key):
-    #TODO
     '''
     Decode cipher with public key.
 
@@ -14,6 +28,10 @@ def decode(cipher, key):
     Return:
         plain: str, plain text
     '''
+    cipher_matrix = text_to_matrix(cipher)
+    public_key = np.array([int(x) for x in key.split()]).reshape(3, 3)
+    private_key = mod_inv(public_key)
+    decode_matrix = np.mod(np.dot(private_key, cipher_matrix), 31)
+    plain = matrix_to_text(decode_matrix)
 
-    plain = ''
     return plain
--- HW2/p2.py
+++ HW2/p2.py
@@ -1,9 +1,23 @@
 import numpy as np
 from util import mod_inv
 
+# ==============================
+
+LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ_.,?!'
+
+# ==============================
+
+def text_to_matrix(text):
+    while len(text) % 3 != 0:
+        text += text[-1]
+    text = [LETTERS.index(i) for i in text]
+    text = np.reshape(text, (-1, 3)).T
+    return text
+
+def matrix_to_text(matrix):
+    return ''.join([LETTERS[x] for x in matrix.T.flatten()])
 
 def get_key(cipher, plain):
-    #TODO
     '''
     Calculate public key with cipher text and plain text.
 
@@ -14,7 +28,10 @@ def get_key(cipher, plain):
     Return:
         key: str, public key
     '''
+    cipher = text_to_matrix(cipher[:9])
+    plain = text_to_matrix(plain[:9])
+    public_key = np.mod(np.dot(cipher, mod_inv(plain)), 31)
 
-    key = ''
+    key = ' '.join([str(x) for x in public_key.flatten()])
     return key
 
